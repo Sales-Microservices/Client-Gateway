387ae9d8c38862c44709ea0fa6afcfe88f0d5c1e
Bernapai <juanemeunier@gmail.com>
2025-09-12 22:31:08 -0300

    Client-Gateway

---
+++ src/products/products.controller.ts
@@ -0,0 +1,59 @@
+import { Controller, Inject, Post, Get, Query, Body, Param, BadRequestException, Patch, Delete } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
+import { PaginationDto, RpcCustomExceptionFilter } from 'src/common';
+import { PRODUCT_SERVICE } from 'src/config';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { CreateProductDto } from './dto/create-product.dto';
+
+
+@Controller('products')
+export class ProductsController {
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) { }
+
+  @Post()
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productsClient.send('createProduct', createProductDto)
+  }
+
+  @Get()
+  getAllProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send('findAllProducts', paginationDto);
+  }
+
+  @Get(':id')
+  async getOneProduct(@Param('id') id: string) {
+    try {
+      const product = await firstValueFrom(
+        this.productsClient.send('findOneProduct', { id })
+      )
+
+      return product;
+    } catch (error) {
+      throw new RpcException(error);
+
+    }
+
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto
+  ) {
+    return this.productsClient.send('updateProduct', {
+      id,
+      ...updateProductDto,
+    }).pipe(
+      catchError(err => { throw new RpcException(err) })
+    )
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: number) {
+    return this.productsClient.send('removeProduct', { id })
+  }
+
+}
